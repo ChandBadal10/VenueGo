13403772e976472e2cdae27f227749fe543aebc6
Badal Chand <badalchand636@gmail.com>
2025-12-06 21:11:19 +0545

    working on my booking page

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,17 +1,68 @@
 import React from 'react'
 
 
+// Dummy datas
+const BOOKINGS = [
+  {
+    id:"BKG-001",
+    sport: "Futsal",
+    venue: "Shantinagar Futsal",
+    date: "2025-12-12",
+    time: "4 pm - 5 pm",
+    location: "Shantinagar, Kathmandu",
+    amount: "Rs 1500",
+    status: "Pending"
+  },
+
+  {
+    id: "BKG-002",
+    sport: "Cricket",
+    venue: "Baneshwor Indoor Nets",
+    date: "2025-02-22",
+    time: "2:00 pm - 4:00 pm",
+    location: "Baneshwor, Kathmandu",
+    amount: "Rs 1700",
+    status: "Completed",
+  },
+
+  {
+    id: "BKG-003",
+    sport: "Basketball",
+    venue: "Velocity Hoops Court",
+    date: "2025-01-05",
+    time: "6:00 pm - 7:30 pm",
+    location: "Ratopul, Kathmandu",
+    amount: "Rs 1600",
+    status: "Completed",
+  },
+
+  {
+    id: "BKG-004",
+    sport: "Table Tennis",
+    venue: "SpinPoint TT Club",
+    date: "2024-12-18",
+    time: "5:30 pm - 6:30 pm",
+    location: "New Baneshwor, Kathmandu",
+    amount: "Rs 500",
+    status: "Completed",
+  },
+
+  {
+    id: "BKG-005",
+    sport: "Futsal",
+    venue: "Velocity Futsal",
+    date: "2024-11-02",
+    time: "7:00 pm - 8:00 pm",
+    location: "Tinkune, Kathmandu",
+    amount: "Rs 1600",
+    status: "Cancelled",
+  },
+];
+
+
 const MyBookings = () => {
   return (
     <div>MyBookings
-
-
-
-
-
-
-
-
 
 
     </div>
